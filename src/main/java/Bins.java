264867f09ff7503916448670bbe5419a5fd92473
Christine <christine@Zipcoders-MacBook-Pro-51.local>
2023-07-07 13:34:47 -0400

    finished bins and it's tested

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,38 @@
 
 public class Bins {
+    // maybe can do this with map (in the future)
+    private int[] binLabels;
+    private int[] binValue;
 
+    // Constructor
+    public Bins(int minVal, int maxVal){
+        this.binLabels = new int[maxVal - minVal + 1];
+        this.binValue = new int[maxVal - minVal + 1];
+
+        setUpBins(minVal);
+    }
+
+    private void setUpBins(int min){
+                for(int i = 0; i < binLabels.length; i++){
+                    binLabels[i] = i + min;
+                    binValue[i] = 0;
+        }
+    }
+
+    public void setBin(Integer binNumber, int value){
+        this.binValue[getIndex(binNumber)] = value;
+    }
+
+    public Integer getBin(Integer binNumber){
+        return this.binValue[getIndex(binNumber)];
+    }
+
+    // Increments bin # 10
+    public void incrementBin(Integer binNumber){
+        binValue[getIndex(binNumber)] = binValue[getIndex(binNumber)] + 1;
+    }
+
+    private int getIndex(int num){
+        return num - binLabels[0];
+    }
 }
--- src/test/java/BinsTest.java
+++ src/test/java/BinsTest.java
@@ -1,2 +1,60 @@
-package PACKAGE_NAME;public class BinsTest {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BinsTest {
+
+    @Test
+    public void testGetBin() {
+        // Given: max number of bins
+        int maxBinLabel = 13;
+
+        // When: bins get created
+        Bins diceHistory = new Bins(0, maxBinLabel);
+
+        // Then: bin value should be 0
+        Assert.assertEquals(new Integer(0), diceHistory.getBin(maxBinLabel));
+    }
+
+    @Test
+    public void testGetBin1() {
+        // Given: number of dice and the max bin label
+        int expectedBinValue = 7;
+        int minBinLabel = 10;
+        int maxBinLabel = 13;
+
+        // When: Roll the dice and increment the bin
+        Bins diceHistory = new Bins(minBinLabel, maxBinLabel);
+        diceHistory.setBin(minBinLabel, expectedBinValue);
+
+        // Then: bin value should increase
+        Assert.assertEquals(new Integer(expectedBinValue), diceHistory.getBin(minBinLabel));
+    }
+
+    @Test
+    public void testIncrementBin() {
+        // Given: number of dice and the max bin label
+        int numOfDice = 1;
+        int maxBinLabel = numOfDice * 6;
+
+        // When: bins get created
+        Bins diceHistory = new Bins(numOfDice * 1, maxBinLabel);
+        diceHistory.incrementBin(maxBinLabel);
+
+        // Then: bin value should be 0
+        Assert.assertEquals(new Integer(1), diceHistory.getBin(maxBinLabel));
+    }
+
+    @Test
+    public void testIncrementBin1() {
+        // Given: number of dice and the max bin label
+        int numOfDice = 5;
+        int maxBinLabel = numOfDice * 6;
+
+        // When: bins get created
+        Bins diceHistory = new Bins(numOfDice * 1, maxBinLabel);
+        diceHistory.incrementBin(maxBinLabel);
+
+        // Then: bin value should be 0
+        Assert.assertEquals(new Integer(1), diceHistory.getBin(maxBinLabel));
+    }
 }
